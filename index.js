5369d7eb2c062df5ea5f18b133993270515b0477
salomemabonga <salome.mabonga@student.moringaschool.com>
2022-06-07 20:45:22 +0300

    commit tree f639396e8874eaeb014ab29657a1c1209ea93481

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun()
+{
+ 
+}
+saturdayFun(roller-skate);
+function saturdayFun(){
+return "This Saturday, I want to roller-skate!";
+}
+function saturdayFun(){
+    return "This Saturday, I want to bathe my dog!";
+}
+
+const mondayWork = function (){
+  console.log("This Monday, I will go to the office.") ;
+
+}
+mondayWork;
+mondayWork('go to the office');
+
+function wrapAdjective(){
+
+}
